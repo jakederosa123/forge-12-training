643c92aa868c6b1bb9be7074fcc0be921f10aa8c
Jacob DeRosa <jakederosa123@gmail.com>
2026-08-11 20:38:24 -0600

    updates

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // The Project URL and Publishable Key are designed to be used in browser code.
 // Never place a service_role key or database password in this file.
 window.FORGE_SYNC_CONFIG = {
-  supabaseUrl: "YOUR_SUPABASE_PROJECT_URL",
-  supabasePublishableKey: "YOUR_SUPABASE_PUBLISHABLE_KEY"
+  supabaseUrl: "https://hlskvqyqncbariudxkkw.supabase.co",
+  supabasePublishableKey: "sb_publishable_6uSZKNBfxxbBb4ybp1CcEA_9NoIFlxg"
 };
 
